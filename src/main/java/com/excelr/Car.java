6e0715cf2a2e0fee3cb054f1483bda95a5c951e5
KondetiBhavadeesh <bhavadeeshkondeti34@gmail.com>
2025-11-18 16:17:31 +0530

    firstspringboot app added

---
+++ src/main/java/com/excelr/Car.java
@@ -0,0 +1,13 @@
+package com.excelr;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Car {
+
+	public Car() {
+		System.out.println("car con exceuted");
+	}
+
+	
+}
---
+++ src/main/java/com/excelr/JulyFirstSpringbootApp1Application.java
@@ -0,0 +1,19 @@
+package com.excelr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class JulyFirstSpringbootApp1Application {
+
+	public static void main(String[] args) {
+		
+		//IOC STARTS
+	ApplicationContext	con=SpringApplication.run(JulyFirstSpringbootApp1Application.class, args);
+	
+	  //getBean() for Car
+	Car c1= con.getBean(Car.class);
+	}
+
+}
